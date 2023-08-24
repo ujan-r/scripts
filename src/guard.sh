7820ce4bf51fae2925bcf526eb62ba02942a8828
Ujan RoyBandyopadhyay <116058173+ujan-r@users.noreply.github.com>
2023-08-24 00:00:43 -0500

    Add guard.sh

---
+++ src/guard.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env sh
+
+# description
+#     add C-style include guards to header files
+#
+# usage
+#     guard [file ...]
+#
+# notes
+#     any files (and intermediate directories) that do not exist will be
+#     created
+#
+#     generated include guards contain a UUID to guarantee uniqueness
+#
+# author
+#     Ujan RoyBandyopadhyay (2023)
+#
+# <https://github.com/ujan-r/scripts>
+
+prefixed() {
+    printf "#ifndef %s\n" "$1"
+    printf "#define %s\n" "$1"
+}
+
+suffixed() {
+    printf "#endif /* !%s */\n" "$1"
+}
+
+for file
+do
+    mkdir -p "$(dirname "$file")" && touch "$file"
+
+    guard="INCLUDE_GUARD_$(uuidgen | tr '-' '_')"
+
+    printf "%s\n\n%s\n\n%s\n"  \
+        "$(prefixed "$guard")" \
+        "$(cat "$file")"       \
+        "$(suffixed "$guard")" \
+    > "$file"
+done
